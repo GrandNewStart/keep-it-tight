89125d6ad862da74831a2de5e303ca222b9ac71c
jinwoo <hbjw123@naver.com>
2025-01-05 21:26:29 -0800

    package name change

--- app/src/main/java/dev/jinwoo/reflection/Theme.kt
+++
@@ -1,5 +0,0 @@
-package dev.jinwoo.reflection
-
-enum class Theme {
-    Light, Dark
-}
--- app/src/main/java/dev/jinwoo/reflection/expense/RecyclerViewItem.kt
+++
@@ -1,8 +0,0 @@
-package dev.jinwoo.reflection.expense
-
-import dev.jinwoo.reflection.db.Expense
-
-sealed class RecyclerViewItem {
-    data class Header(val date: String) : RecyclerViewItem()
-    data class ExpenseItem(val expense: Expense) : RecyclerViewItem()
-}
